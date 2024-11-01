26a751d4f2f9c4e418f9b2a01e5c1c8058cf0af7
Matheus Fillipe <matheusfillipeag@gmail.com>
2024-11-01 13:48:54 +0100

    fixed the bible?

--- plugins/bible.py
+++ plugins/bible.py
@@ -4,23 +4,6 @@ from curl_cffi.requests.exceptions import HTTPError
 from cloudbot import hook
 from cloudbot.util import formatting
 
-headers = {
-    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-    "accept-language": "en-US,en;q=0.9,pt-BR;q=0.8,pt;q=0.7,de;q=0.6",
-    "cache-control": "no-cache",
-    "pragma": "no-cache",
-    "priority": "u=0, i",
-    "sec-ch-ua": '"Google Chrome";v="129", "Not=A?Brand";v="8", "Chromium";v="129"',
-    "sec-ch-ua-mobile": "?0",
-    "sec-ch-ua-platform": '"macOS"',
-    "sec-fetch-dest": "document",
-    "sec-fetch-mode": "navigate",
-    "sec-fetch-site": "none",
-    "sec-fetch-user": "?1",
-    "upgrade-insecure-requests": "1",
-    "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36",
-}
-
 
 @hook.command("bible", "passage", singlethread=True)
 def bible(text, reply):
@@ -31,7 +14,7 @@ def bible(text, reply):
         r = requests.get(
             "https://labs.bible.org/api/",
             params=params,
-            headers=headers,
+            impersonate="chrome124",
         )
         r.raise_for_status()
         response = r.json()[0]
